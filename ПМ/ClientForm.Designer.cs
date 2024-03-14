4d41e1ed587547b0b08e8b7a0e67136bb0ba75e7
PROCuuuba <barkalov.ilia@gmail.com>
2024-03-14 21:56:08 +0300

    Добавьте файлы проекта.

---
+++ ПМ/ClientForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ПМ
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.изменитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.npgsqlDataAdapter1 = new Npgsql.NpgsqlDataAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 50);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Форма заполнения телефона на ремонт";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tan;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Snow;
+            this.button1.Location = new System.Drawing.Point(314, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 55);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDate.ForeColor = System.Drawing.Color.Snow;
+            this.labelDate.Location = new System.Drawing.Point(210, 324);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(134, 29);
+            this.labelDate.TabIndex = 16;
+            this.labelDate.Text = "label6";
+            // 
+            // labelTime
+            // 
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.ForeColor = System.Drawing.Color.Snow;
+            this.labelTime.Location = new System.Drawing.Point(210, 353);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(134, 29);
+            this.labelTime.TabIndex = 17;
+            this.labelTime.Text = "label7";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Tan;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(696, 243);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.изменитьToolStripMenuItem,
+            this.удалитьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(148, 52);
+            // 
+            // изменитьToolStripMenuItem
+            // 
+            this.изменитьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.изменитьToolStripMenuItem.Name = "изменитьToolStripMenuItem";
+            this.изменитьToolStripMenuItem.Size = new System.Drawing.Size(147, 24);
+            this.изменитьToolStripMenuItem.Text = "Изменить";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 27);
+            this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(147, 24);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.Color.Tan;
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUpdate.ForeColor = System.Drawing.Color.Snow;
+            this.buttonUpdate.Location = new System.Drawing.Point(51, 327);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(153, 50);
+            this.buttonUpdate.TabIndex = 19;
+            this.buttonUpdate.Text = "Обновить";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // npgsqlDataAdapter1
+            // 
+            this.npgsqlDataAdapter1.DeleteCommand = null;
+            this.npgsqlDataAdapter1.InsertCommand = null;
+            this.npgsqlDataAdapter1.SelectCommand = null;
+            this.npgsqlDataAdapter1.UpdateCommand = null;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ClientForm";
+            this.ShowIcon = false;
+            this.Text = "PhoneFix";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClientForm_FormClosing);
+            this.Load += new System.EventHandler(this.ClientForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem изменитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private Npgsql.NpgsqlDataAdapter npgsqlDataAdapter1;
+    }
+}
---
+++ ПМ/ClientForm.cs
@@ -0,0 +1,137 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ПМ
+{
+    public partial class ClientForm : Form
+    {
+        PostgresConnector bd = new PostgresConnector();
+
+        public ClientForm()
+        {
+            InitializeComponent();
+
+            labelDate.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            labelTime.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.CellMouseDown += dataGridView1_CellMouseDown;
+
+            bd.openConnection();
+            string queryString = "SELECT client_fio, client_telephone_number, problem, date_of_the_end, phone_name, cost FROM client_info";
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(queryString, bd.getConnection());
+            DataTable dataTable = new DataTable();
+            adapter.FillSchema(dataTable, SchemaType.Source);
+            dataGridView1.DataSource = dataTable;
+
+            dataGridView1.Columns["client_fio"].HeaderText = "ФИО клиента";
+            dataGridView1.Columns["client_telephone_number"].HeaderText = "Телефон клиента";
+            dataGridView1.Columns["problem"].HeaderText = "Проблема";
+            dataGridView1.Columns["date_of_the_end"].HeaderText = "Дата окончания";
+            dataGridView1.Columns["phone_name"].HeaderText = "Название телефона";
+            dataGridView1.Columns["cost"].HeaderText = "Стоимость";
+
+            bd.closeConnection();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            DataTable dataTable = (DataTable)dataGridView1.DataSource;
+
+            string updateQueryString = "SELECT client_info_id, client_fio, client_telephone_number, problem, date_of_the_end, phone_name, cost FROM client_info";
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(updateQueryString, bd.getConnection());
+            NpgsqlCommandBuilder builder = new NpgsqlCommandBuilder(adapter);
+            adapter.Update(dataTable);
+
+            string selectQueryString = "SELECT client_info_id, client_fio, client_telephone_number, problem, date_of_the_end, phone_name, cost FROM client_info";
+            NpgsqlCommand cmd = new NpgsqlCommand(selectQueryString, bd.getConnection());
+            bd.openConnection();
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+                dataGridView1.DataSource = dataTable;
+                dataGridView1.Columns["client_info_id"].Visible = false;
+
+                dataGridView1.Columns["client_fio"].HeaderText = "ФИО клиента";
+                dataGridView1.Columns["client_telephone_number"].HeaderText = "Телефон клиента";
+                dataGridView1.Columns["problem"].HeaderText = "Проблема";
+                dataGridView1.Columns["date_of_the_end"].HeaderText = "Дата окончания";
+                dataGridView1.Columns["phone_name"].HeaderText = "Название телефона";
+                dataGridView1.Columns["cost"].HeaderText = "Стоимость";
+            }
+            finally
+            {
+                reader.Close();
+                bd.closeConnection();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Menu f1 = new Menu();
+            f1.ShowDialog();
+        }
+
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                bd.openConnection();
+                e.SuppressKeyPress = true;
+                contextMenuStrip1.Close();
+                int clientId = (int)dataGridView1.CurrentRow.Cells["client_info_id"].Value;
+
+                NpgsqlCommand cmd = new NpgsqlCommand($"UPDATE client_info SET {dataGridView1.CurrentCell.OwningColumn.HeaderText} = '{toolStripTextBox1.Text}'" +
+                    $"WHERE client_info_id = {clientId}", bd.getConnection());
+                cmd.ExecuteNonQuery();
+                toolStripTextBox1.Clear();
+                string queryString = "SELECT client_info_id, client_fio, client_telephone_number, problem, date_of_the_end, phone_name, cost FROM client_info";
+                NpgsqlCommand cmd1 = new NpgsqlCommand(queryString, bd.getConnection());
+                NpgsqlDataReader reader = cmd1.ExecuteReader();
+                try
+                {
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+                    dataGridView1.DataSource = dataTable;
+                    dataGridView1.Columns["client_info_id"].Visible = false;
+                }
+                finally
+                {
+                    reader.Close();
+                    bd.closeConnection();
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                dataGridView1.CurrentCell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bd.openConnection();
+            int clientId = (int)dataGridView1.CurrentRow.Cells["client_info_id"].Value;
+            string deleteString = $"DELETE FROM client_info WHERE client_info_id = {clientId}";
+            NpgsqlCommand deleteCommand = new NpgsqlCommand(deleteString, bd.getConnection());
+            deleteCommand.ExecuteNonQuery();
+            bd.closeConnection();
+        }
+    }
+}
